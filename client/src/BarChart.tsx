0091ed79dd30e6535222ce5526a322d4f060249a
Filip <fifomihal@gmail.com>
2021-01-29 16:07:42 +0100

    new hint + conditional coloring

--- client/src/BarChart.tsx
+++ client/src/BarChart.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Hint, VerticalBarSeries, XAxis, XYPlot, YAxis } from 'react-vis'
 import './BarChart.css'
+import { getDateFormat } from './StatsService'
 interface IProps {
     chartData: Array<{ x: number; y: number }>
 }
@@ -13,30 +14,29 @@ export default function BarChart(props: IProps) {
 
     return (
         <div style={{ position: 'relative' }}>
-            <XYPlot height={400} width={1000} stackBy="y" xDomain={[0, 30]} yDomain={[0, 800]}>
-                {/* TODO: show day/month */}
-                <XAxis title={'last 30 days'} />
+            <XYPlot height={400} xType="time" width={1400} stackBy="y" yDomain={[0, 800]}>
+                <XAxis title={'last 30 days'} tickTotal={10} tickFormat={getDateFormat} />
                 <YAxis title={'screen time/minutes'} />
                 {hoveredPoint && (
-                    // TODO: redesign hint
                     <Hint value={hoveredPoint}>
                         <div className={'barChartTooltip'}>
                             <h3>Screen time</h3>
                             <p>
-                                {hoveredPoint.y} mins on {hoveredPoint.x}
+                                {hoveredPoint.y} mins on {getDateFormat(hoveredPoint.x)}
                             </p>
                         </div>
                     </Hint>
                 )}
-                {/* TODO: Add conditional color for bars which reach certain threshold */}
                 <VerticalBarSeries
+                    colorType={'literal'}
+                    color={'#0f0'}
                     onValueMouseOver={(data) => {
                         setHoveredPoint(data as { x: number; y: number })
                     }}
                     onValueMouseOut={() => {
                         setHoveredPoint(undefined)
                     }}
-                    barWidth={0.6}
+                    barWidth={0.5}
                     data={props.chartData}
                 />
             </XYPlot>
